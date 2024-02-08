9102501224aef6b48c1f5ff9811204b5e858ead4
Telocea <ff1estelle@gmail.com>
2024-02-08 00:04:59 -0500

    16 Set up library page with some styling

--- app/app/routes/library.$userId.tsx
+++ app/app/routes/library.$userId.tsx
@@ -1,149 +1,198 @@
 import * as React from 'react';
-import type { LinksFunction } from "@remix-run/node";
 import {   
-    Form,
     Link,
-    LiveReload,
-    Outlet,
     Meta,
     Links,
-    Scripts,
-    useLoaderData,
-    ScrollRestoration, } from '@remix-run/react';
+    useLoaderData, } from '@remix-run/react';
 import { Button,
     Typography,
-    IconButton,
-    TextField,
+    Box,
+    createTheme,
+    InputBase,
+    AppBar,
+    styled,
+    alpha,
+    Grid,
+    Paper,
 } from '@mui/material';
-import { json } from "@remix-run/node";
-import { NoEncryption } from '@mui/icons-material';
+import { 
+    Search as SearchIcon,
+    AutoStories,
+} from '@mui/icons-material';
 
-import { getBooks } from "../models/book.server";
+const theme = createTheme({
+    palette: {
+      background: {
+        paper: '#fff',
+      },
+      text: {
+        primary: '#0c174b',
+        secondary: '#fff',
+      },
+      primary: {
+        main: '#de7d37'
+      }
+    },
+    shape: {
+      borderRadius: 50,
+    }
+});
 
-export const loader = async () => {
-    const books = await getBooks();
-    return json({ books });
-  };
+const fakeBooks = [
+  {
+    _id: 1,
+    title: 'Book1',
+    author: "author1",
+    genre: "genre1",
+    description: "",
+    image: "",
+    usersId: 1,
+  },
+  {
+    _id: 2,
+    title: 'Book2',
+    author: "author2",
+    genre: "genre2",
+    description: "",
+    image: "",
+    usersId: 1,
+  },
+  {
+    _id: 3,
+    title: 'Book3',
+    author: "author3",
+    genre: "genre3",
+    description: "",
+    image: "",
+    usersId: 1,
+  },
+];
+
+const BookCard = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#d3d3d3',
+  padding: theme.spacing(1),
+  borderRadius: 15,
+  height: 140,
+  width: 100,
+}));
 
 
-export default function Library() {
-  const { books } = useLoaderData();
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: 100,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  border: '2px solid #0c174b',
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%'
+}));
 
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+  },
+}));
+
+export default function Library() {  
   return (
-    <React.Fragment>
-        <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <Meta />
-            <Links />
-        </head>
-        <div id="sidebar">
-            <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-                Library
-            </Typography>
-            <Button variant="contained" component={Link} to="/BookUploadBook">
-                Upload a book
-            </Button>
-            <div
-            style={{
-                display: "flex",
-                alignSelf: "center",
-                justifyContent: "center",
-                flexDirection: "column",
-                padding: 20
-            }}
-            >
-            <form>
-            <TextField
-                id="search-bar"
-                className="text"
-                label="Search for a book"
-                variant="outlined"
-                placeholder="Search..."
-                size="small"
-            />
-                <IconButton type="submit" aria-label="search">
-            </IconButton>
-            </form>
-            </div>
-
-            <nav>
-            {books.length ? (
-                <ul>
-                    {books.map((book) => (
-                    <li className="text"
-                    style={{
-                    padding: 5,
-                    justifyContent: "normal",
-                    fontSize: 18,
-                    color: "blue",
-                    margin: 1,
-                    width: "250px",
-                    borderWidth: "10px",
-                    textDecoration: "none",
-                    listStyleType: "none",
-                    fontFamily: "sans-serif"
-                    }}
-                    key={book.id} >
-                        <Link to={`${book.id}` }  style={{ textDecoration: 'none' }}
-                        >
-                        {book.title || book.author ? (
-                            <>
-                            {book.title} by {book.author}
-                            </>
-                        ) : (
-                            <i>No title</i>
-                        )}{" "}
-                        </Link>
-                    </li>
-                    ))}
-                </ul>
-                ) : (
-                <p>
-                    <i>No books</i>
-                </p>
-                )}
-            </nav>
-
-            <div
-            style={{
-                display: "flex",
-                flexDirection: "row",
-                flexWrap: "wrap",
+    <Box
+          sx={{
+              flexGrow: 1,
+          }}
+      >
+        <AppBar position="static">
+            <Box sx={{
+                display: 'flex',
+                flexDirection:'column',
+                bgcolor: '#de7d37'
             }}>
-                {books.length ? (
-                <table>
-                    <tbody>
-                        <tr style={{
-                            padding: "15px"
-                        }} >
-                        {books.map((book) => (
-                        <th style={{
-                            padding: "15px"
-                        }} className="covers"
-                        key={book.id} >
-                            <div>
-                                {/* <img 
-                                alt={`${book.title} by ${book.author} avatar`}
-                                key={book.image}
-                                src={book.image}
-                                width="200" 
-                                height="250"
-                                /> */}
-                            </div>
-                        </th>
-                    ))}
-                        </tr>
-                    </tbody>
-                    
-                </table>
-                ) : (
-                <p>
-                    <i>No books</i>
-                </p>
-                )}
-            </div>
-        </div>
-    </React.Fragment>
-    
+                <Box
+                    alignItems={"center"}
+                    display={"flex"}
+                    justifyContent={"center"}
+                    padding={"10px"}
+                >
+                  <Typography
+                        variant="h4"
+                        fontWeight='bold'
+                        style={{ wordWrap: "break-word" }}
+                        component="div"
+                        display="block"
+                        color="#0c174b"
+                        padding="30px 0px 0px 0px"
+                    >
+                    Library
+                    </Typography>
+                </Box>
+                
+                <Box sx={{
+                    padding: '15px 30px 15px 30px',
+                }}>
+                <Search>
+                    <SearchIconWrapper>
+                        <SearchIcon />
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                    placeholder="Search"
+                    inputProps={{ 'aria-label': 'search' }}
+                    />
+                </Search>
+                </Box>
+            </Box>
+        </AppBar>
+              
+        <Box sx={{
+          padding: "10px",
+          alignItems: "right"
+        }}>
+          <Button sx={{
+            borderRadius: 10,
+            bgcolor: "#0c174b",
+            color: "#fff",
+          }}
+              variant="contained" component={Link} to="/BookUploadBook">
+              Upload a book
+          </Button>
+        </Box>
+
+        <Box sx={{ 
+          flexGrow: 1,
+          padding: "10px",
+        }}>
+          <Grid container sx={{ flexGrow: 1 }} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <Grid container justifyContent="center" spacing={5}>
+              {[0, 1, 3, 4, 5, 6, 7, 8].map((value) => (
+                <Grid key={value} item>
+                  <BookCard sx={{ 
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center"
+                  }}>
+                    <AutoStories fontSize='large'/>
+                  </BookCard>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Box>
+    </Box>
   );
 }
