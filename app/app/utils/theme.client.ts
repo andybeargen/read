6eb3e2c381bc74b3352fd5071ec6732bd942aff5
logonoff <git@logonoff.co>
2024-02-17 04:18:16 -0500

    reduce text size

--- app/app/utils/theme.client.ts
+++ app/app/utils/theme.client.ts
@@ -14,31 +14,31 @@ export const CrittersTheme = createTheme({
   },
   typography: {
     h1: {
-      fontSize: "2.5em",
+      fontSize: "2.5rem",
       fontWeight: 500,
       lineHeight: 1.2,
       letterSpacing: "-0.00833em",
     },
     h2: {
-      fontSize: "2em",
+      fontSize: "2rem",
       fontWeight: 500,
       lineHeight: 1.2,
       letterSpacing: "-0.00833em",
     },
     h3: {
-      fontSize: "1.75em",
+      fontSize: "1.75rem",
       fontWeight: 500,
       lineHeight: 1.2,
       letterSpacing: "-0.00833em",
     },
     h4: {
-      fontSize: "1.5em",
+      fontSize: "1.5rem",
       fontWeight: 500,
       lineHeight: 1.2,
       letterSpacing: "-0.00833em",
     },
     h5: {
-      fontSize: "1.25em",
+      fontSize: "1.25rem",
       lineHeight: 1.2,
       letterSpacing: "-0.00833em",
     },
