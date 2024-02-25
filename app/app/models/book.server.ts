eb003049fbbad14c65a733e737783716e9b39d7a
Matteo Giannone <matteo.giannone@mail.utoronto.ca>
2024-02-25 16:59:57 -0500

    Define a type for the parameter of createBook to improve readability

--- app/app/models/book.server.ts
+++ app/app/models/book.server.ts
@@ -4,6 +4,16 @@ import { prisma } from "~/db.server";
 
 export type { Book } from "@prisma/client";
 
+export type BookCreationData = {
+    title: string,
+    author: string,
+    genre: string,
+    description: string,
+    file: Buffer,
+    image: Buffer | null,
+    user: {connect: {id: string}}
+};
+
 export async function getBookById(id: Book["id"]) {
   return prisma.book.findUnique({ where: { id } });
 }
@@ -12,7 +22,7 @@ export async function getUserLibrary(userId: User["id"]) {
   return prisma.book.findMany({ where: { userId } });
 }
 
-export async function createBook(values: {title: string, author: string, genre: string, description: string, file: Buffer, image: Buffer | null, user: {connect: {id: string}}}) {
+export async function createBook(values: BookCreationData) {
   return prisma.book.create({
     data: values,
   });
