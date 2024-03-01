f73a22b88772a38ad65d270117e0891e1cfbf78d
Maksym-W <113399768+Maksym-W@users.noreply.github.com>
2024-03-01 00:06:37 -0500

    18 landing page (#64)
    * Created the landing page.
    
    * Remove colour bug which caused other pages to be same colour as the landing page.

--- app/app/routes/_index.tsx
+++ app/app/routes/_index.tsx
@@ -1,7 +1,9 @@
-import { Typography } from "@mui/material";
+import { Box, Container, Typography, Button } from "@mui/material";
 import type { LoaderFunction, MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 import { authenticator } from "~/utils/auth.server";
+import './landing.css'; // The css file for the landing page
+
 
 export const meta: MetaFunction = () => {
   return [
@@ -22,12 +24,42 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function Index() {
   return (
-    <>
-      <Typography component="h1" variant="h1">
-        Start reading
+    <Box className="index-page-box" display="flex" flexDirection="column" alignItems="center" justifyContent="center" minHeight="100vh">
+      <Container className="indexx" style={{
+        height: '50vh', // full viewport height
+        display: 'flex',
+        alignItems: 'center', // vertical alignment
+        justifyContent: 'center', // horizontal alignment
+      }}>
+        <img
+          src="/pikachuonbooks.png"
+          alt="critter"
+          style={{ maxWidth: '100%', maxHeight: '100%' }} // to ensure the image is responsive and does not overflow
+        />
+      </Container>
+
+      <Typography component="h1" variant="h1" textAlign="center">
+        Lit Critters
       </Typography>
-      <Link to={"/login"}>Login</Link>
-      <Link to={"/register"}>Register</Link>
-    </>
+
+      <Typography component="h5" variant="h5" textAlign="center">
+        Where reading lit books gets you the Littist of Critters. 
+        Just upload a pdf/epub and read away!
+      </Typography>
+
+      <Box mt={3} mb={2}>
+        <Link to="/login" style={{ textDecoration: 'none', margin: '10px' }}>
+          <Button variant="contained">
+            Login
+          </Button>
+        </Link>
+
+        <Link to="/register" style={{ textDecoration: 'none', margin: '10px' }}>
+          <Button variant="contained">
+            Register
+          </Button>
+        </Link>
+      </Box>
+    </Box>
   );
 }
