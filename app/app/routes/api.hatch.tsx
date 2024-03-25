7a6f40b955b70061d00531975e0a6515353b8f20
Matteo Giannone <matteo.giannone@mail.utoronto.ca>
2024-03-25 00:03:42 -0400

    Improve visuals of hatching critter and also decrement coins when hatching

--- app/app/routes/api.hatch.tsx
+++ app/app/routes/api.hatch.tsx
@@ -1,6 +1,7 @@
 import { LoaderFunctionArgs, json, redirect } from "@remix-run/node";
 import { prisma } from "~/db.server";
 import { hatchCritter } from "~/models/critter.server";
+import { addCoins, removeCoins } from "~/models/user.server";
 import { authenticator } from "~/utils/auth.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -22,9 +23,14 @@ export async function loader({ request }: LoaderFunctionArgs) {
     return json({error: "No user"});
   }
 
+  if (userData.coins < 500) {
+    return json({ error: "Insufficient amount of coins to hatch critter" })
+  }
+
   let critter = await hatchCritter(userData.id);
   if (critter) {
+    await removeCoins(userData.id, 500);
     return json({ critter });
   }
-  return json({ error: "No critter"});
+  return json({ error: "Unable to hatch critter"});
 }
--- app/app/routes/hatchery.tsx
+++ app/app/routes/hatchery.tsx
@@ -1,33 +1,143 @@
+import { Box, Container, Modal, Typography } from "@mui/material";
 import { User } from "@prisma/client";
 import { ActionFunction, ActionFunctionArgs, LoaderFunction, redirect } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
+import { useState } from "react";
+import { CoinIcon } from "~/components";
 import { AuthenticatedLayout } from "~/components/AuthenticatedLayout";
 import { prisma } from "~/db.server";
 import { hatchCritter } from "~/models/critter.server";
 import { authenticator } from "~/utils/auth.server";
 
-async function hatchCritterAndNotify() {
-  const response = await fetch("http://localhost:3000/api/hatch");
-  const responseJson = await response.json();
-  const error = responseJson?.error;
-  const critter = responseJson?.critter;
-
-  if (error) {
-    alert(error);
-  } else {
-    alert("Obtained " + critter.name);
-  }
-}
+const CoinCount = ({ count }: { count: number }) => {
+  return (
+    <Box
+      display={"flex"}
+      flexDirection={"row"}
+      alignItems={"center"}
+      gap={1}
+      bgcolor={"#0045bd"}
+      borderRadius={"30px"}
+      px={1}
+      py={0.5}
+    >
+      <Typography
+        fontFamily={"monospace"}
+        variant="h6"
+        component="h1"
+        color={"white"}
+      >
+        {count}
+      </Typography>
+      <CoinIcon style={{ width: "30px" }} />
+    </Box>
+  );
+};
 
 export default function HatcheryRoute() {
   let user: User = useLoaderData<typeof loader>();
+  const [open, setOpen] = useState(false);
+  const [hatchedCritter, setHatchedCritter] = useState("Kelpxolotl");
+  const handleClose = () => setOpen(false);
+
+  async function hatchCritterAndNotify() {
+    if (!user || user.coins < 500) {
+      alert("Insufficient amount of coins to hatch critter");
+      return;
+    }
+
+    const response = await fetch("http://localhost:3000/api/hatch");
+    const responseJson = await response.json();
+    const error = responseJson?.error;
+    const critter = responseJson?.critter;
+
+    if (error) {
+      alert(error);
+    } else {
+      setHatchedCritter(critter.name);
+      setOpen(true);
+      user.coins -= 500;
+    }
+  }
 
   return (
     <AuthenticatedLayout>
-      <h1>
-        Hatchery
-      </h1>
-      <button onClick={hatchCritterAndNotify}>Hatch</button>
+      <Modal open={open} onClose={handleClose} sx={{ '& .MuiBackdrop-root': { backgroundColor: 'rgba(0,0,0,0.9)' } }}>
+        <Container sx={{position: 'absolute' as 'absolute', top: "50%", left: "50%", display: "flex", transform: "translate(-50%, -50%)", width: 600, height: 600, justifyContent: "space-between", flexDirection: "column", alignItems: "center"}}>
+          <Typography variant="h1" component="h1" fontFamily={"monospace"} fontWeight={"bold"} color="#E89B60">You hatched a</Typography>
+          <img
+              src={`/critters/${hatchedCritter}.png`}
+              alt="critter"
+            />
+          <Typography variant="h1" component="h1" fontFamily={"monospace"} fontWeight={"bold"} color="#98C9FF">{hatchedCritter}</Typography>
+        </Container>
+      </Modal>
+      <Container
+        sx={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          style={{
+            background: "linear-gradient(to top, #E89B60, #98C9FF)",
+            height: "50vh",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            zIndex: -1,
+            borderRadius: "0 0 9999% 9999%",
+          }}
+        />
+
+        <Container
+          sx={{
+            display: "flex",
+            height: "7%",
+            justifyContent: "space-between",
+            maxWidth: "1200px",
+            padding: 0,
+            position: "relative",
+            top: "2rem",
+          }}
+        >
+          <Container></Container>
+          <CoinCount count={user.coins} />
+        </Container>
+        <Container sx={{display: "flex", marginTop: 10, height: "100%", justifyContent: "space-between", flexDirection: "column", alignItems: "center"}}>
+          <Typography variant="h1" component="h1" fontFamily={"monospace"} fontWeight={"bold"}>Hatchery</Typography>
+          <img
+              src={`/Egg.png`}
+              alt="critter"
+            />
+          <div onClick={hatchCritterAndNotify} style={{width: "100%", display: "flex", justifyContent: "center"}}>
+            <Box
+                bgcolor={"#D9F4FF"}
+                fontSize={"2em"}
+                gap={2}
+                alignItems={"center"}
+                display={"flex"}
+                flexDirection={"row"}
+                justifyContent={"center"}
+                width={"80%"}
+                height={"80px"}
+                borderRadius={"50px"}
+                border={"2px solid #E89B60"}
+                color={"#0045bd"}
+                mb={20}
+                sx={{ "&:hover": { backgroundColor: "#001e6b", color: "white" } }}
+              >
+                <Typography variant="h4" component="h1" fontFamily={"monospace"}>
+                  HATCH for 500
+                </Typography>
+                <CoinIcon style={{ width: "40px", marginTop: "-5px" }} />
+            </Box>
+          </div>
+        </Container>
+      </Container>
     </AuthenticatedLayout>
   );
 }
