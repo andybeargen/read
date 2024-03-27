37f07c073ba81e082e88d9eb3ce6cdff5efa1dc1
Daniyal Bokhari <daniyalcraft123@gmail.com>
2024-03-26 22:17:40 -0400

    rmeove comment

--- app/app/models/critter.server.ts
+++ app/app/models/critter.server.ts
@@ -9,58 +9,6 @@ export async function getCritterById(id: Critter["id"]) {
   return prisma.critter.findUnique({ where: { id } });
 }
 
-// This is your Prisma schema file,
-// learn more about it in the docs: https://pris.ly/d/prisma-schema
-
-// generator client {
-//   provider = "prisma-client-js"
-// }
-
-// datasource db {
-//   provider = "postgresql"
-//   url      = env("DATABASE_URL")
-// }
-
-// model Book {
-//   id          String         @id @default(uuid()) @map("_id")
-//   title       String
-//   author      String
-//   genre       String?
-//   description String
-//   file        Bytes
-//   image       Bytes?
-//   user       User            @relation(fields: [userId], references: [id])
-//   userId     String
-// }
-
-// model User {
-//   id                         String              @id @default(uuid()) @map("_id")
-//   email                      String              @unique
-//   username                   String
-//   password                   String
-//   coins                      Int                 @default(0)
-//   books                      Book[]
-//   dateCreated                DateTime            @default(now())
-//   UserCritter                UserCritter[]
-// }
-
-// model Critter {
-//   id          String @id @default(uuid()) @map("_id")
-//   name        String @unique
-//   description String
-//   type        String
-//   UserCritter UserCritter[]
-// }
-
-// model UserCritter {
-//   id        String @id @default(uuid()) @map("_id")
-//   level     Int @default(1)
-//   xp        Int @default(0)
-//   userId    String
-//   critterId String
-//   user      User @relation(fields: [userId], references: [id])
-//   critter   Critter @relation(fields: [critterId], references: [id])
-// }
 
 export async function createCritter(
   name: Critter["name"],
