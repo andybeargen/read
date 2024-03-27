f6dd63c87b4de8a5252ba1e0746f6681cdf96060
estelle <84874560+telocea@users.noreply.github.com>
2024-03-26 22:15:51 -0400

    37 reader (#88)
    * Display example book
    
    * Option description for epub uploads
    
    * Zoom, view current chapter page
    
    * display page number, sans-serif font
    
    * Correct book rendered
    
    * Remove local save progress
    
    * Fix cover page bug
    
    * Fix dependencies
    
    * Update package.json

--- app/app/models/book.server.ts
+++ app/app/models/book.server.ts
@@ -1,13 +1,20 @@
 import type { User, Book } from "@prisma/client";
+import * as fs from 'fs';
 
 import { prisma } from "~/db.server";
-
+import { Buffer} from "buffer";
 export type { Book } from "@prisma/client";
+
 
 export type BookCreationData = Omit<Book, "userId" | "id"> & {user: {connect: {id: string}}};
 
 export async function getBookById(id: Book["id"]) {
-  return prisma.book.findUnique({ where: { id } });
+  const book = await prisma.book.findUnique({ where: { id } });
+  const filePath = "public/files/current.epub";
+
+  fs.writeFileSync(filePath, Buffer.from(book.file));
+  
+  return book;
 }
 
 export async function getUserLibrary(userId: User["id"]) {
---
+++ app/prisma/migrations/20240326185226_optional_desc/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Book" ALTER COLUMN "description" DROP NOT NULL;
