82a37d1e28cd404183fb0bee9907d6098c554448
Matteo Giannone <matteo.giannone@mail.utoronto.ca>
2024-04-03 15:00:34 -0400

    Add critter info page to collection route

--- app/app/routes/collection.tsx
+++ app/app/routes/collection.tsx
@@ -4,9 +4,10 @@ import { authenticator } from "~/utils/auth.server";
 import { getUserCritters } from "~/models/critter.server";
 import { redirect } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import { AppBar, Box, Card, CardContent, Grid, InputBase, Typography, styled } from "@mui/material";
-import { Search as SearchIcon } from "@mui/icons-material";
+import { AppBar, Box, Button, Card, CardContent, Container, Grid, IconButton, InputBase, Typography, styled } from "@mui/material";
+import { Search as SearchIcon, ArrowBack as BackIcon } from "@mui/icons-material";
 import { useState } from "react";
+import { CoinIcon } from "~/components";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -38,9 +39,35 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
+const CoinCount = ({ count }: { count: number }) => {
+  return (
+    <Box
+      display={"flex"}
+      flexDirection={"row"}
+      alignItems={"center"}
+      gap={1}
+      bgcolor={"#0045bd"}
+      borderRadius={"30px"}
+      px={1}
+      py={0.5}
+    >
+      <Typography
+        fontFamily={"monospace"}
+        variant="h6"
+        component="h1"
+        color={"white"}
+      >
+        {count}
+      </Typography>
+      <CoinIcon style={{ width: "30px" }} />
+    </Box>
+  );
+};
+
 export default function CollectionRoute() {
-  const { critters } = useLoaderData();
+  const { critters, user } = useLoaderData<typeof loader>();
   const [searchItem, setSearchItem] = useState("");
+  const [currentCritter, setCurrentCritter] = useState(null);
 
   const handleInputChange = (e: { target: { value: any } }) => {
     const searchTerm = e.target.value;
@@ -55,81 +82,182 @@ export default function CollectionRoute() {
 
   return (
     <AuthenticatedLayout>
-      <AppBar position="static" elevation={0}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            bgcolor: "#de7d37",
-          }}
-        >
-          <Box
-            alignItems={"center"}
-            display={"flex"}
-            justifyContent={"center"}
-            padding={"10px"}
-          >
-            <Typography
-              variant="h4"
-              style={{ wordWrap: "break-word" }}
-              color="#0c174b"
-              padding="30px 0px 0px 0px"
+      {!currentCritter
+        ? (<>
+          <AppBar position="static" elevation={0}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                bgcolor: "#de7d37",
+              }}
             >
-              Critter Collection
-            </Typography>
-          </Box>
-
-          <Box
+              <Box
+                alignItems={"center"}
+                display={"flex"}
+                justifyContent={"center"}
+                padding={"10px"}
+              >
+                <Typography
+                  variant="h4"
+                  style={{ wordWrap: "break-word" }}
+                  color="#0c174b"
+                  padding="30px 0px 0px 0px"
+                >
+                  Critter Collection
+                </Typography>
+              </Box>
+  
+              <Box
+                sx={{
+                  padding: "15px 30px",
+                }}
+              >
+                <Search>
+                  <SearchIconWrapper>
+                    <SearchIcon />
+                  </SearchIconWrapper>
+                  <StyledInputBase
+                    placeholder="Search"
+                    inputProps={{ "aria-label": "search" }}
+                    value={searchItem}
+                    onChange={handleInputChange}
+                  />
+                </Search>
+              </Box>
+            </Box>
+          </AppBar>
+  
+          <Grid container spacing={2} justifyContent='left' alignItems='flex-start' sx={{ paddingX: '30%', paddingTop: '3rem', paddingBottom: '7rem' }}>
+            {getFilteredCritters(critters).map((critter) => (
+              <Grid item xs={12} sm={6} md={4} key={critter.id}>
+                <div onClick={() => { setCurrentCritter(critter) }} style={{cursor: 'pointer'}}>
+                  <Card  sx={{ 
+                    border: '1px solid #de7d37', 
+                    boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)', 
+                    borderRadius: '15px',
+                    backgroundImage: 'linear-gradient(to right, #FFCC80, #80D8FF)'
+                  }}>
+                    <Box sx={{ display: 'flex', justifyContent: 'center', paddingY: '1rem', height: '160px' }}>
+                      <img 
+                      src={`/critters/${critter.critter.image}`} 
+                      alt={critter.critter.name} 
+                      style={{ maxWidth: '160px', maxHeight: '160px' }}
+                      />
+                    </Box>
+                    <CardContent sx={{backgroundColor: '#fff'}}>
+                      <Typography variant="h3" sx={{ mt: 1, color: '#0c174b' }}>
+                        {critter.critter.name}
+                      </Typography>
+                      <Typography variant="h6" sx={{ mt: 1 }}>
+                        Type: {critter.critter.type}
+                      </Typography>
+                      <Typography variant="h6">
+                        Level: {critter.level}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </div>
+              </Grid>
+            ))}
+          </Grid>
+        </>)
+        : (<>
+          <Container
             sx={{
-              padding: "15px 30px",
+              height: "100vh",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search"
-                inputProps={{ "aria-label": "search" }}
-                value={searchItem}
-                onChange={handleInputChange}
-              />
-            </Search>
-          </Box>
-        </Box>
-      </AppBar>
+            <Box
+              style={{
+                background: "linear-gradient(to top, #E89B60, #98C9FF)",
+                height: "50vh",
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right: 0,
+                zIndex: -1,
+                borderRadius: "0 0 9999% 9999%",
+              }}
+            />
 
-      <Grid container spacing={2} justifyContent='left' alignItems='flex-start' sx={{ paddingX: '30%', paddingTop: '3rem', paddingBottom: '7rem' }}>
-        {getFilteredCritters(critters).map((critter) => (
-          <Grid item xs={12} sm={6} md={4} key={critter.id}>
-            <Card sx={{ 
-              border: '1px solid #de7d37', 
-              boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)', 
-              borderRadius: '15px',
-              backgroundImage: 'linear-gradient(to right, #FFCC80, #80D8FF)'
-            }}>
-              <Box sx={{ display: 'flex', justifyContent: 'center', paddingY: '1rem', height: '160px' }}>
-                <img 
-                  src={`/critters/${critter.critter.image}`} 
-                  alt={critter.critter.name} 
-                  style={{ maxWidth: '160px', maxHeight: '160px' }}
-                />
-              </Box>
-              <CardContent sx={{backgroundColor: '#fff'}}>
-                <Typography variant="h3" sx={{ mt: 1, color: '#0c174b' }}>
-                  {critter.critter.name}
-                </Typography>
-                <Typography variant="h6" sx={{ mt: 1 }}>
-                  Type: {critter.critter.type}
-                </Typography>
-                <Typography variant="h6">
-                  Level: {critter.level}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+            <Container
+              sx={{
+                display: "flex",
+                height: "7%",
+                justifyContent: "space-between",
+                maxWidth: "1200px",
+                padding: 0,
+                position: "relative",
+                top: "2rem",
+              }}
+            >
+              <IconButton
+                aria-label="settings"
+                style={{ height: "100%" }}
+                component={Button}
+                onClick={ () => setCurrentCritter(null) }
+              >
+                <BackIcon sx={{ height: "100%", position: "relative", fill: "#0045bd", transform: "scale(1.8)" }} />
+              </IconButton>
+
+              <CoinCount count={user.coins} />
+            </Container>
+
+            <img
+              src={`/critters/${currentCritter.critter.image}`}
+              alt="critter"
+              style={{ maxWidth: "300px", marginTop: "7%" }}
+            />
+            <Typography
+              variant="h3"
+              component="h1"
+              fontFamily={"monospace"}
+              fontWeight={"bold"}
+              color={"#0045bd"}
+              sx={{marginTop: "5rem", marginBottom: "1rem"}}
+            >
+              {currentCritter.critter.name}
+            </Typography>
+            <Typography
+              variant="h5"
+              component="h1"
+              fontFamily={"monospace"}
+              fontWeight={"bold"}
+              color={"#0045bd"}
+            >
+              Type: {currentCritter.critter.type}
+            </Typography>
+            <Typography
+              variant="h5"
+              component="h1"
+              fontFamily={"monospace"}
+              fontWeight={"bold"}
+              color={"#0045bd"}
+              sx={{marginBottom: "1rem"}}
+            >
+              Level: {currentCritter.level}
+            </Typography>
+            <Typography
+              variant="h5"
+              component="h1"
+              fontFamily={"monospace"}
+              fontWeight={"bold"}
+              color={"#0045bd"}
+              sx={{marginBottom: "1rem"}}
+            >
+              Description
+            </Typography>
+            <Typography sx={{textAlign: 'center'}}>
+              {currentCritter.critter.description}
+            </Typography>
+          </Container>
+        </>)
+      }
+      
     </AuthenticatedLayout>
   );
 }
@@ -146,5 +274,5 @@ export const loader: LoaderFunction = async ({ request }) => {
   }
  // get all the user's critters
   const critters = await getUserCritters(user.id);
-  return { critters };
+  return { critters, user };
 };
